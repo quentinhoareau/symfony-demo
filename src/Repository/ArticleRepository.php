614fb47392b52284a51bf7ffebaed0fcbb7e18bb
Quentin Hoareau <hoareauquentin97480@gmail.com>
2021-02-01 23:16:07 +0400

    Update ArticleRepository.php

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -11,6 +11,7 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Article|null findOneBy(array $criteria, array $orderBy = null)
  * @method Article[]    findAll()
  * @method Article[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Article[]    articleLimit(azazazz)
  */
 class ArticleRepository extends ServiceEntityRepository
 {
@@ -19,22 +20,17 @@ class ArticleRepository extends ServiceEntityRepository
         parent::__construct($registry, Article::class);
     }
 
-    // /**
-    //  * @return Article[] Returns an array of Article objects
-    //  */
-    /*
-    public function findByExampleField($value)
+
+
+    /**
+     * @return Article[]
+     */
+
+    public function articleEnabled(): array
     {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
+        //
     }
-    */
+
 
     /*
     public function findOneBySomeField($value): ?Article
