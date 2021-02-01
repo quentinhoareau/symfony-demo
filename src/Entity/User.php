0ecef95518d739fb3be0a579693b3b6b5d1fa7ce
Quentin Hoareau <hoareauquentin97480@gmail.com>
2021-02-01 21:17:04 +0400

    update

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,14 +3,22 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * User
  *
  * @ORM\Table(name="user")
  * @ORM\Entity
+ * @UniqueEntity(
+ *     fields={"email"},
+ *     message="Cet email est déjà utilisé"
+ * )
+ * 
  */
-class User
+class User implements UserInterface
 {
     /**
      * @var int
@@ -39,9 +47,66 @@ class User
      * @var string
      *
      * @ORM\Column(name="password", type="string", length=255, nullable=false)
+     * @Assert\Length(min="8", minMessage="Votre message doit faire au minimum 8 caractères")
+     * @Assert\EqualTo(propertyPath="confirm_password", message="Vos mots de passe ne sont pas identiques.")
      */
     private $password;
 
+    /**
+     * @var string
+     * @Assert\EqualTo(propertyPath="password", message="Vos mots de passe ne sont pas identiques.")
+     */
     public $confirm_password;
 
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+    public function getSalt()
+    {
+    }
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
 }
--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -2,27 +2,27 @@
 
 namespace App\Form;
 
-use App\Entity\User;
+use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class RegistrationType extends AbstractType
+class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('username')
-            ->add('password', PasswordType::class)
-            ->add('confirm_password', PasswordType::class);
+            ->add('author')
+            ->add('content')
+            ->add('createdAt')
+            ->add('article')
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+            'data_class' => Comment::class,
         ]);
     }
 }
